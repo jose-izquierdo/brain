05083b2cfc8dd3245f627114bbc911ecc03f567d
Jose Izquierdo <jose@camillion.app>
2021-07-16 14:29:46 +0200

    rubocop config for Travis completed. All rubocop offenses corrected

--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :news do
-    category { "MyString" }
-    datetime { "" }
-    headline { "MyString" }
+    category { 'MyString' }
+    datetime { '' }
+    headline { 'MyString' }
     external_id { 1 }
-    image { "MyString" }
-    related { "MyString" }
-    source { "MyString" }
-    summary { "MyText" }
-    url { "MyString" }
+    image { 'MyString' }
+    related { 'MyString' }
+    source { 'MyString' }
+    summary { 'MyText' }
+    url { 'MyString' }
   end
 end
--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -1,4 +1,6 @@
-RSpec.configure do |config|
+# frozen_string_literal: true
+
+RSpec.configure do
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       # Choose a test framework:
