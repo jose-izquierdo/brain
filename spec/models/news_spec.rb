d02c7098d7cf615f2fc8bf18c2826b80f4a5e6e7
Jose Izquierdo <jose@camillion.app>
2021-07-17 17:31:22 +0200

    validations of presence eliminated sor far from news model except :external_id

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -5,6 +5,5 @@ require 'rails_helper'
 RSpec.describe News, type: :model do
   describe 'Validations' do
     it { should validate_uniqueness_of(:external_id) }
-    it { should validate_presence_of(:external_id) }
   end
 end
